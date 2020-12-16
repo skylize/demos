7b2741a1d101d948c80255da38d89fd047378517
Sky Higgins <eveningsky@gmail.com>
2020-12-16 15:29:05 -0500

    Create App.js

---
+++ react-infinite-scroll/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.css';
+
+const App = () => (
+  <div className='App'>
+    <h1>Hello</h1>
+    <h2>I don't do much yet...</h2>
+  </div>
+)
+
+export default App
