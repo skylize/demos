740216f4c8cce474bcc88900b9a77ca1e5ac73c8
Sky Higgins <eveningsky@gmail.com>
2020-12-18 20:51:50 -0500

    Create InfiniteScroll HOC Moved all the infinite scroll logic to its own component.

--- react-infinite-scroll/src/App.js
+++ react-infinite-scroll/src/App.js
@@ -1,14 +1,15 @@
 import React, {
   createContext,
+  useCallback,
   useContext,
   useEffect,
   useReducer,
-  useRef,
   useState,
 } from 'react'
 import './styles.css'
 import styles from './styles.js'
 import { fetchData } from './fake-fetch-data'
+console.clear()
 
 const actions = {
   start: 'start',
@@ -47,7 +48,7 @@ const FetchDataProvider = ({ children }) => {
   const load = () => {
     dispatch({ type: actions.start })
     more &&
-      fetchData({ page, limit }).then(newData =>
+      fetchData({ page, limit: limit }).then(newData =>
         dispatch({
           type: actions.loaded,
           newData,
@@ -61,32 +62,58 @@ const FetchDataProvider = ({ children }) => {
   )
 }
 
-const LoadMore = () => {
-  const { loading, more, load } = useContext(FetchDataCtx)
-  const [element, setElement] = useState()
-  const loader = useRef(load)
+const InfiniteScroll = ({
+  loadFn,
+  moreTest,
+  observerOpts = { threshold: 0.2 },
+  children,
+  ...props
+}) => {
+  const [target, setTarget] = useState()
 
-  const observer = useRef(
-    new IntersectionObserver(
-      entries => entries[0].isIntersecting && loader.current(),
-      { threshold: 0.2 },
-    ),
+  const ref = useCallback(
+    node => {
+      console.log({ node, target, is: Object.is(node, target) })
+      if (!Object.is(node, target))
+        setTarget(node) && console.log('setting target')
+    },
+    [target, setTarget],
   )
 
   useEffect(() => {
-    loader.current = load
-  }, [load])
+    if (!target || !moreTest || !loadFn) return
 
-  useEffect(() => {
-    const el = element
-    const ob = observer.current
+    const currentTarget = target
 
-    el && ob.observe(el)
+    const onIntersect = entries => {
+      console.log({ entries, target, moreTest, loadFn })
+      const t = entries.find(
+        e => e.isIntersecting && Object.is(e.target, target),
+      )
+      t && moreTest && moreTest() && loadFn && loadFn()
+    }
 
-    return () => el && ob.unobserve(el)
-  }, [element])
+    const observer = new IntersectionObserver(onIntersect, observerOpts)
 
-  return !loading && more && <li ref={setElement} style={styles.btnMoreLi} />
+    currentTarget && observer.observe(currentTarget)
+    return () => currentTarget && observer.unobserve(currentTarget)
+  }, [target, moreTest, loadFn, observerOpts])
+
+  return <div ref={ref}>{children}</div>
+}
+
+const LoadMore = () => {
+  const { loading, more, load } = useContext(FetchDataCtx)
+  const [moreTest, setMoreTest] = useState(null)
+  useEffect(() => setMoreTest(_ => () => more && !loading), [loading, more])
+
+  return (
+    <li>
+      <InfiniteScroll loadFn={load} moreTest={moreTest}>
+        <div />
+      </InfiniteScroll>
+    </li>
+  )
 }
 
 const Loading = () => {
@@ -109,10 +136,12 @@ const Loader = () => {
   )
 }
 
-export default App = () => (
+const App = () => (
   <FetchDataProvider>
     <div className='App'>
       <Loader />
     </div>
   </FetchDataProvider>
 )
+
+export default App
